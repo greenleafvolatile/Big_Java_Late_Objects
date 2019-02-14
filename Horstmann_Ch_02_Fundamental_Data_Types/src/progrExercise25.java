a81a19bc5523214aed552e10680ff48b1347e50b
Daan Pol <daanpol@tutamail.com>
2019-02-14 10:27:16 +0100

    Added programming exercises 25

---
+++ Horstmann_Ch_02_Fundamental_Data_Types/src/progrExercise25.java
@@ -0,0 +1,31 @@
+/**
+ * "Enhance the output of Excersise P2.4 so that the numbers are properly aligned."
+ */
+
+import java.util.Scanner;
+
+public class progrExercise25 {
+
+    // This program prompts the user for two numbers and then prints the sum, the difference, the product, the average, the distance, the maximum and the minimu.
+
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner (System.in);
+
+        System.out.print("Please enter an number: ");
+
+        double firstNumber = in.nextDouble();
+
+        System.out.print("Please enter another number: ");
+
+        double secondNumber = in.nextDouble();
+
+        System.out.printf("The sum of the numbers you entered is:             %10.2f \n", (firstNumber + secondNumber));
+        System.out.printf("The difference between the numbers you entered is: %10.2f \n", (firstNumber - secondNumber));
+        System.out.printf("The product of the numbers you entered is:         %10.2f \n", (firstNumber * secondNumber));
+        System.out.printf("The average of the numbers you entered is:         %10.2f \n", (firstNumber + secondNumber) / 2);
+        System.out.printf("The distance of the numbers you entered is:        %10.2f \n", Math.abs(firstNumber - secondNumber));
+        System.out.printf("The maximum of the two numbers you entered is:     %10.2f \n", Math.max(firstNumber, secondNumber));
+        System.out.printf("The minimum of the two numbers you entered is:     %10.2f \n", Math.min(firstNumber, secondNumber));
+    }
+}
