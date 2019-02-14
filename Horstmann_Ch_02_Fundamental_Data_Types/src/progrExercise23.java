e7800d0de9a9144ee5ec39e00f05832b76fe73fc
Daan Pol <daanpol@tutamail.com>
2019-02-14 10:14:27 +0100

    Added programming exercises 23

---
+++ Horstmann_Ch_02_Fundamental_Data_Types/src/progrExercise23.java
@@ -0,0 +1,20 @@
+/**
+ * "Write a program that reads a number and displays the square, cube and fourth power. Use the Math.pow method only for the fourth power."
+ */
+
+import java.util.Scanner;
+
+public class progrExercise23 {
+
+    // This program gives the square, cube and fourth power of a give number.
+
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please enter a number:");
+        double number = in.nextInt();
+        System.out.printf("Your number squared is:              %.2f \n", number * number);
+        System.out.printf("Your number cubed is:                %.2f \n", number * number * number);
+        System.out.printf("Your number to the fourth power is:  %.2f", Math.pow(number, 4));
+    }
+}
