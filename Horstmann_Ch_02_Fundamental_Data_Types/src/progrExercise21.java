7003bae0a2191eb33384a8b22560dace40c3a5e5
Daan Pol <daanpol@tutamail.com>
2019-05-20 21:35:37 +0200

    Added programming exercise 2.1

--- Horstmann_Ch_02_Fundamental_Data_Types/src/progrExercise21.java
+++ Horstmann_Ch_02_Fundamental_Data_Types/src/progrExercise21.java
@@ -5,11 +5,9 @@
 
 public class progrExercise21 {
 
-    // This program displays the dimension of a letter size sheet of paper in millimeters
-
     public static void main(String[] args) {
 
-        final double INCH = 25.4; // one inch is 25.4 millimeters. This value should not be altered which is why I made it a constant.
+        final double INCH = 25.4;
         double width = 8.5;
         double height = 11;
         System.out.printf("The width in millimeters is:   %.2f \n", width * INCH);
