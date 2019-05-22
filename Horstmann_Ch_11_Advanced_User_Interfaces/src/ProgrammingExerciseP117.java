231abf75f47b486ba28e718b43235e846951c07e
Daan Pol <daanpol@tutamail.com>
2019-05-22 19:38:05 +0200

    Added Programming Exercise P11.7

--- Horstmann_Ch_11_Advanced_User_Interfaces/src/ProgrammingExerciseP117.java
+++ Horstmann_Ch_11_Advanced_User_Interfaces/src/ProgrammingExerciseP117.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class ProgrammingExerciseP117 extends JFrame { //
+public class ProgrammingExerciseP117 extends JFrame {
 
     private final RectangleComponent3 rectangleComponent;
 
