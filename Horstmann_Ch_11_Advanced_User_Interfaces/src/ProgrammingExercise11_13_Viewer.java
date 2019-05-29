8f1eb5321ddb1fe1f116bf9f15ebb4bfc1ee98cf
Daan Pol <daanpol@tutamail.com>
2019-05-29 14:48:30 +0200

    Added Programming Exercise P11.13 Viewer

--- Horstmann_Ch_11_Advanced_User_Interfaces/src/ProgrammingExercise11_13_Viewer.java
+++ Horstmann_Ch_11_Advanced_User_Interfaces/src/ProgrammingExercise11_13_Viewer.java
@@ -5,11 +5,11 @@ import javax.swing.*;
  * edges of the component rather than simply moving outside."
  */
 
-public class ProgrammingExercise1113_Viewer {
+public class ProgrammingExercise11_13_Viewer {
 
     public static void main(String[] args){
 
-        JFrame frame=new ProgrammingExercise1113_Frame();
+        JFrame frame=new ProgrammingExercise11_13_Frame();
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
