e65b874ab84a9a1d01f2da2febfd31d797f2557a
Daan Pol <daanpol@tutamail.com>
2019-05-29 14:43:23 +0200

    Added Programming Exercise P11.13 Component

---
+++ Horstmann_Ch_11_Advanced_User_Interfaces/src/ProgrammingExercise1113_Component.java
@@ -0,0 +1,45 @@
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+
+public class ProgrammingExercise1113_Component extends JComponent{
+
+    private static final int DIAMETER=25, INIT_XPOS=20, INIT_YPOS=50;
+    private int xPos, yPos;
+
+    public ProgrammingExercise1113_Component(){
+        final int BORDER_THICKNESS=2;
+        final Color BORDER_COLOR= Color.BLACK;
+        this.setBorder(new LineBorder(BORDER_COLOR, BORDER_THICKNESS));
+        this.xPos=INIT_XPOS;
+        this.yPos=INIT_YPOS;
+    }
+
+    public void paintComponent(Graphics g){
+        g.fillOval(xPos, yPos, DIAMETER, DIAMETER);
+
+    }
+
+    public void moveRectangleBy(int dx, int dy){
+        this.xPos+=dx;
+        this.yPos+=dy;
+        repaint();
+    }
+
+    public int getXpos(){
+        return this.xPos;
+    }
+
+    public int getYpos(){
+        return this.yPos;
+    }
+
+    public int getDiameter(){
+        return DIAMETER;
+    }
+    @Override
+    public Dimension getPreferredSize(){
+        return new Dimension(300, 300);
+    }
+
+}
