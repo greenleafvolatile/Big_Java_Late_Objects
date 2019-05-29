f2574b09ce1892e4dcaf328bbbc9defb42e124ce
Daan Pol <daanpol@tutamail.com>
2019-05-29 14:41:39 +0200

    Added Programming Exercise P11.13 Viewer

---
+++ Horstmann_Ch_11_Advanced_User_Interfaces/src/ProgrammingExercise1113_Viewer.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+/**
+ * "Change the rectangleComponent for the animation in Section 11.5 so that the rectangle bounces off the
+ * edges of the component rather than simply moving outside."
+ */
+
+public class ProgrammingExercise1113_Viewer {
+
+    public static void main(String[] args){
+
+        JFrame frame=new ProgrammingExercise1113_Frame();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
