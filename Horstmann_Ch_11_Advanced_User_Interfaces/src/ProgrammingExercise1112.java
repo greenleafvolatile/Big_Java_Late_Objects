06d3793f3ce205c23b3cfe561d486e350a305979
Daan Pol <daanpol@tutamail.com>
2019-05-29 11:53:33 +0200

    Added Programming Exercise P11.12

---
+++ Horstmann_Ch_11_Advanced_User_Interfaces/src/ProgrammingExercise1112.java
@@ -0,0 +1,25 @@
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Write a program that uses a timer to print the current time once a second.
+ */
+
+public class ProgrammingExercise1112 {
+
+    public static void main(String[] args){
+
+        class printTime extends TimerTask {
+
+            public void run(){
+                DateTimeFormatter formatter=DateTimeFormatter.ofPattern("HH:mm:ss");
+                System.out.println(formatter.format(LocalTime.now()));
+            }
+        }
+        Timer t=new Timer();
+        t.schedule(new printTime(), 0, 1000);
+    }
+}
+
