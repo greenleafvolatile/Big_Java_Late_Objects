f1399195c3294d935e441181c007c1c11ffa4b80
Daan Pol <daanpol@tutamail.com>
2019-06-01 12:21:17 +0200

    Added Programming Exercise P11.15 Car Component

---
+++ Horstmann_Ch_11_Advanced_User_Interfaces/src/Programming_Exercise_11_15_Car_Component.java
@@ -0,0 +1,32 @@
+import java.util.List;
+import java.awt.*;
+import javax.swing.*;
+
+public class Programming_Exercise_11_15_Car_Component extends JComponent {
+
+    public static final int PREF_WIDTH=400;
+    public static final int PREF_HEIGHT=400;
+
+    //private final Programming_Exercise_11_15_Car carOne;
+    //private final Programming_Exercise_11_15_Car carTwo;
+
+    private final List<Programming_Exercise_11_15_Car> cars;
+
+    public  Programming_Exercise_11_15_Car_Component(List<Programming_Exercise_11_15_Car> cars){
+        this.cars=cars;
+
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        for(int i=0;i<cars.size();i++){
+            cars.get(i).drawCar(g);
+        }
+    }
+
+
+    public Dimension getPreferredSize(){
+        return new Dimension(PREF_WIDTH, PREF_HEIGHT);
+    }
+
+}
