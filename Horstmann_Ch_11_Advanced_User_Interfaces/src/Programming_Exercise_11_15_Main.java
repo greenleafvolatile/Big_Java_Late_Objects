9f961c091b0e56b762019da82d8fb09e453ec371
Daan Pol <daanpol@tutamail.com>
2019-06-01 12:20:52 +0200

    Added Programming Exercise P11.15 Main

--- Horstmann_Ch_11_Advanced_User_Interfaces/src/Programming_Exercise_11_15_Main.java
+++ Horstmann_Ch_11_Advanced_User_Interfaces/src/Programming_Exercise_11_15_Main.java
@@ -1,44 +1,101 @@
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Programming_Exercise_11_15_Main extends JFrame {
 
-    public Programming_Exercise_11_15_Main(){
+    boolean driveRight, driveLeft;
+    List<Programming_Exercise_11_15_Car> cars;
+    Programming_Exercise_11_15_Car_Component carComponent;
+
+    private Programming_Exercise_11_15_Main(){
+        driveLeft=true;
+        driveRight=false;
+
+        cars=new ArrayList<>();
+
+        cars.add(new Programming_Exercise_11_15_Car(new Point(200, 50), driveRight));
+
+        cars.add(new Programming_Exercise_11_15_Car(new Point(200, 150), driveLeft));
+
         JPanel contPanel=new JPanel();
         this.setContentPane(contPanel);
+
         Color borderColor=Color.BLACK;
         int borderThickness=2;
         contPanel.setBorder(new LineBorder(borderColor, borderThickness));
-        Programming_Exercise_11_15_Car_Component carComponent=new Programming_Exercise_11_15_Car_Component();
-        carComponent.setCarOneInitPos(new Point(0, carComponent.getPreferredSize().height*1/4));
-        carComponent.setCarTwoInitPos(new Point(carComponent.getPreferredSize().width-Programming_Exercise_11_15_Car.getCarWidth(), carComponent.getPreferredSize().height*3/4));
+
+
+        //Programming_Exercise_11_15_Car carOne=new Programming_Exercise_11_15_Car(new Point(200, 50), driveRight);
+        //Programming_Exercise_11_15_Car carTwo=new Programming_Exercise_11_15_Car(new Point(200-Programming_Exercise_11_15_Car.WIDTH, 100),driveLeft);
+
+        carComponent=new Programming_Exercise_11_15_Car_Component(cars);
 
         contPanel.add(carComponent);
+
         this.pack();
-        int carOneSpeed=1;
-        int carTwoSpeed=-1;
-        Timer t1=new Timer(100, new ActionListener(){
+
+        int delay=50;
+
+        Timer t1=new Timer(delay, new CarListener());/*new ActionListener(){
 
             public void actionPerformed(ActionEvent event){
-                carComponent.moveCarOne(carOneSpeed);
-            }
-        });
-        Timer t2=new Timer(100, new ActionListener() {
+                if(carOne.getXpos()==0){
+                    carOne.setDirection(Programming_Exercise_11_15_Main.this.driveRight);
+                }
+                else if(carOne.getXpos()+Programming_Exercise_11_15_Car.WIDTH==carComponent.getPreferredSize().width-1){
+                    carOne.setDirection(driveLeft);
 
-            public void actionPerformed(ActionEvent event) {
-                carComponent.moveCarTwo(carTwoSpeed);
+                }
+                carOne.drive();
+                carComponent.repaint();
+
             }
         });
+
+        Timer t2=new Timer(delay, new ActionListener(){
+
+            public void actionPerformed(ActionEvent event){
+                if(carTwo.getXpos()==0){
+                    carTwo.setDirection(driveRight);
+                }
+                else if(carTwo.getXpos()+Programming_Exercise_11_15_Car.WIDTH==carComponent.getPreferredSize().width-1){
+                    scarTwo.setDirection(driveLeft);
+                }
+                carTwo.drive();
+                carComponent.repaint();
+
+            }
+        });*/
         t1.start();
-        t2.start();
-
+        //t2.start();
     }
+
+    class CarListener implements ActionListener{
+
+        public void actionPerformed(ActionEvent event){
+            for(int i=0;i<cars.size();i++){
+                if(cars.get(i).getXpos()==0){
+                    cars.get(i).setDirection(driveRight);
+                }
+                else if(cars.get(i).getXpos()+Programming_Exercise_11_15_Car.WIDTH==carComponent.getPreferredSize().width-1){
+                    cars.get(i).setDirection(driveLeft);
+                }
+                cars.get(i).drive();
+                carComponent.repaint();
+
+            }
+        }
+    }
+
 
     public static void main(String[] args){
         JFrame carFrame=new Programming_Exercise_11_15_Main();
+        carFrame.setLocationRelativeTo(null);
         carFrame.setVisible(true);
         carFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
