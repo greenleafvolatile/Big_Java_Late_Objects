9e58ef9df58630553cbc6268ec7720f300771e48
Daan Pol <daanpol@tutamail.com>
2019-06-03 09:40:49 +0200

    Added Programming Exercise 11.11 Main

--- Horstmann_Ch_11_Advanced_User_Interfaces/src/Programming_Exercise_11_11_Main.java
+++ Horstmann_Ch_11_Advanced_User_Interfaces/src/Programming_Exercise_11_11_Main.java
@@ -21,7 +21,7 @@ import java.awt.*;
  * Use appropriate components to ask for the length, label, and color, then apply them when the user clicks an "Add item" button"."
  */
 
-public class ProgrammingExercise1111_Main extends JFrame {
+public class Programming_Exercise_11_11_Main extends JFrame {
 
     private final JPanel contPanel;
     private JLabel nameLabel, lengthLabel;
@@ -30,16 +30,16 @@ public class ProgrammingExercise1111_Main extends JFrame {
     private String name;
     private int length;
     private JTextField nameField, lengthField;
-    private final ProgrammingExercise1111_Chart_Component chartComponent;
+    private final Programming_Exercise_11_11_Chart_Component chartComponent;
 
-    private ProgrammingExercise1111_Main(){
+    private Programming_Exercise_11_11_Main(){
         final Color BORDER_COLOR = Color.BLACK;
         final int BORDER_THICKNESS = 2;
         contPanel=new JPanel();
         this.setContentPane(contPanel);
         contPanel.setLayout(new BorderLayout());
         contPanel.setBorder(new TitledBorder(new LineBorder(BORDER_COLOR, BORDER_THICKNESS),"Chart Designer Version 1.01", TitledBorder.CENTER, TitledBorder.ABOVE_TOP));
-        chartComponent=new ProgrammingExercise1111_Chart_Component();
+        chartComponent=new Programming_Exercise_11_11_Chart_Component();
         contPanel.add(chartComponent, BorderLayout.CENTER);
         this.createLabels();
         this.createTextFields();
@@ -62,35 +62,35 @@ public class ProgrammingExercise1111_Main extends JFrame {
 
     private void createButtons() {
         colorButton = new JButton("Select item color");
-        colorButton.addActionListener(event -> ProgrammingExercise1111_Main.this.color = JColorChooser.showDialog(ProgrammingExercise1111_Main.this.contPanel, "Select a color", null));
+        colorButton.addActionListener(event -> Programming_Exercise_11_11_Main.this.color = JColorChooser.showDialog(Programming_Exercise_11_11_Main.this.contPanel, "Select a color", null));
 
         drawButton = new JButton("Add item");
         drawButton.addActionListener(event -> {
             try {
-                name = ProgrammingExercise1111_Main.this.nameField.getText();
+                name = Programming_Exercise_11_11_Main.this.nameField.getText();
                 if (name.equals("")) {
                     throw new Exception("Please enter a name!");
                 }
 
-                length = Integer.parseInt(ProgrammingExercise1111_Main.this.lengthField.getText().length() == 0 ? "0" : ProgrammingExercise1111_Main.this.lengthField.getText());
-                if (length == 0 || length<0 || length> ProgrammingExercise1111_Main.this.chartComponent.getPreferredSize().width) {
+                length = Integer.parseInt(Programming_Exercise_11_11_Main.this.lengthField.getText().length() == 0 ? "0" : Programming_Exercise_11_11_Main.this.lengthField.getText());
+                if (length == 0 || length<0 || length> Programming_Exercise_11_11_Main.this.chartComponent.getPreferredSize().width) {
                     throw new Exception("Please enter a valid bar length!");
                 }
-                ProgrammingExercise1111_Item item = new ProgrammingExercise1111_Item(name, length, ProgrammingExercise1111_Main.this.color);
-                ProgrammingExercise1111_Main.this.chartComponent.addBar(item);
+                Programming_Exercise_11_11_Item item = new Programming_Exercise_11_11_Item(name, length, Programming_Exercise_11_11_Main.this.color);
+                Programming_Exercise_11_11_Main.this.chartComponent.addBar(item);
             }
             catch (Exception e) {
                 if (e instanceof NumberFormatException) {
-                    JOptionPane.showMessageDialog(ProgrammingExercise1111_Main.this, "Length must be a number", "Error!", JOptionPane.ERROR_MESSAGE);
+                    JOptionPane.showMessageDialog(Programming_Exercise_11_11_Main.this, "Length must be a number", "Error!", JOptionPane.ERROR_MESSAGE);
                 } else {
-                    JOptionPane.showMessageDialog(ProgrammingExercise1111_Main.this, e.getMessage(), "Error!", JOptionPane.ERROR_MESSAGE);
+                    JOptionPane.showMessageDialog(Programming_Exercise_11_11_Main.this, e.getMessage(), "Error!", JOptionPane.ERROR_MESSAGE);
                 }
             }
 
         });
 
         resetButton = new JButton("Reset");
-        resetButton.addActionListener(e -> ProgrammingExercise1111_Main.this.chartComponent.reset());
+        resetButton.addActionListener(e -> Programming_Exercise_11_11_Main.this.chartComponent.reset());
     }
 
     private void createControlPanel(){
@@ -120,7 +120,7 @@ public class ProgrammingExercise1111_Main extends JFrame {
     }
 
     public static void main(String[] args) {
-        JFrame frame = new ProgrammingExercise1111_Main();
+        JFrame frame = new Programming_Exercise_11_11_Main();
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
