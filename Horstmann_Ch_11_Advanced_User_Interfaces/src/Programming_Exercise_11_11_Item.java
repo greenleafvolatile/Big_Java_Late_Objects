30c2e859d242252ea3ba7e880b3a49465d9e2695
Daan Pol <daanpol@tutamail.com>
2019-06-03 09:46:09 +0200

    Added Programming Exercise 11.11 Item

--- Horstmann_Ch_11_Advanced_User_Interfaces/src/Programming_Exercise_11_11_Item.java
+++ Horstmann_Ch_11_Advanced_User_Interfaces/src/Programming_Exercise_11_11_Item.java
@@ -1,12 +1,12 @@
 import java.awt.*;
 
-public class ProgrammingExercise1111_Item {
+public class Programming_Exercise_11_11_Item {
 
     private final int length;
     private final Color color;
     private final String name;
 
-    public ProgrammingExercise1111_Item(String name, int length, Color color){
+    public Programming_Exercise_11_11_Item(String name, int length, Color color){
         this.name=name;
         this.color=color;
         this.length=length;
