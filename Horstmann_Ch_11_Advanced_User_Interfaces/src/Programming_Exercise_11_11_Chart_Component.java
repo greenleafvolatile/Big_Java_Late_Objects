2b73f9cb5ba987388b4f343787f28d6fd7107499
Daan Pol <daanpol@tutamail.com>
2019-06-03 09:46:26 +0200

    Added Programming Exercise 11.11 Chart Component

--- Horstmann_Ch_11_Advanced_User_Interfaces/src/Programming_Exercise_11_11_Chart_Component.java
+++ Horstmann_Ch_11_Advanced_User_Interfaces/src/Programming_Exercise_11_11_Chart_Component.java
@@ -3,15 +3,15 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.swing.*;
 
-public class ProgrammingExercise1111_Chart_Component extends JComponent{
+public class Programming_Exercise_11_11_Chart_Component extends JComponent{
 
     private static final int PREF_WIDTH=400;
     private static final int PREF_HEIGHT=800;
 
-    private final List<ProgrammingExercise1111_Item> items;
+    private final List<Programming_Exercise_11_11_Item> items;
 
 
-    public ProgrammingExercise1111_Chart_Component(){
+    public Programming_Exercise_11_11_Chart_Component(){
         this.items=new ArrayList<>();
     }
 
@@ -21,18 +21,18 @@ public class ProgrammingExercise1111_Chart_Component extends JComponent{
         final int ITEM_HEIGHT=20, GAP=20;
 
         int y=GAP;
-        for (ProgrammingExercise1111_Item item : items) {
+        for (Programming_Exercise_11_11_Item item : items) {
             g.setColor(Color.BLACK);
             g.drawString(item.getName(), 0, y);
             g.setColor(item.getColor());
             g.fillRect(0, y + 5,item.getLength(), ITEM_HEIGHT);
             y += GAP + ITEM_HEIGHT;
-            if (y > ProgrammingExercise1111_Chart_Component.PREF_HEIGHT) {
+            if (y > Programming_Exercise_11_11_Chart_Component.PREF_HEIGHT) {
 
                 SwingUtilities.invokeLater(new Runnable() {
                     @Override
                     public void run() {
-                        JOptionPane.showMessageDialog(ProgrammingExercise1111_Chart_Component.this, "Cannot not add anymore items", "Error!", JOptionPane.ERROR_MESSAGE);
+                        JOptionPane.showMessageDialog(Programming_Exercise_11_11_Chart_Component.this, "Cannot not add anymore items", "Error!", JOptionPane.ERROR_MESSAGE);
                     }
                 });
             }
@@ -44,7 +44,7 @@ public class ProgrammingExercise1111_Chart_Component extends JComponent{
         return new Dimension(PREF_WIDTH, PREF_HEIGHT);
     }
 
-    public void addBar(ProgrammingExercise1111_Item bar){
+    public void addBar(Programming_Exercise_11_11_Item bar){
         items.add(bar);
         repaint();
     }
