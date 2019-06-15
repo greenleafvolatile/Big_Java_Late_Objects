d1226c1e5f4529a8043624e305be555824ac6414
Daan Pol <daanpol@tutamail.com>
2019-06-15 10:11:53 +0200

    Added Programming Exercise 11.17 Component

--- Horstmann_Ch_11_Advanced_User_Interfaces/src/Programming_Exercise_11_17_Component.java
+++ Horstmann_Ch_11_Advanced_User_Interfaces/src/Programming_Exercise_11_17_Component.java
@@ -1,24 +1,23 @@
 import java.awt.Canvas;
 import java.awt.*;
-import java.util.logging.Logger;
 import javax.swing.*;
 
 public class Programming_Exercise_11_17_Component extends JComponent{
 
-    public static final int PREF_WIDTH = 400, PREF_HEIGHT = 400;
+    public static final int PREF_WIDTH = 400;
+    public static final int PREF_HEIGHT = 400;
 
-    private String_Metrics_Calculator calculator;
-    private Programming_Exercise_11_17_Direction direction;
+    private final Programming_Exercise_11_17_Direction direction;
     private int xPos, yPos;
     private String message;
 
     public Programming_Exercise_11_17_Component(String message, Font font) {
-        this.setFont(font);
+        setFont(font);
         this.message = message;
-        this.direction = new Programming_Exercise_11_17_Direction("right", "down");
+        direction = new Programming_Exercise_11_17_Direction("right", "down");
         final Point INIT_POSITION=new Point(0-new Canvas().getFontMetrics(this.getFont()).stringWidth(this.message), 0);
-        this.xPos=(int) INIT_POSITION.getX();
-        this.yPos=(int) INIT_POSITION.getY();
+        xPos=(int) INIT_POSITION.getX();
+        yPos=(int) INIT_POSITION.getY();
     }
 
     @Override
@@ -28,48 +27,43 @@ public class Programming_Exercise_11_17_Component extends JComponent{
         g2d.drawString(message, xPos, yPos);
     }
 
-    public String getMessage(){
-        return this.message;
-    }
-
     public void setMessage(String message){
-        Logger.getGlobal().info("Check!");
         this.message=message;
-        this.repaint();
+        repaint();
     }
 
     public int getMessageHeight() {
-        return new Canvas().getFontMetrics(this.getFont()).stringWidth(this.message);
+        return new Canvas().getFontMetrics(getFont()).stringWidth(message);
     }
 
     public int getMessageWidth(){
-        return new Canvas().getFontMetrics(this.getFont()).stringWidth(this.message);
+        return new Canvas().getFontMetrics(getFont()).stringWidth(message);
     }
 
     public void moveMessage() {
-        if (this.direction.getVerticalDirection().equals("down") && this.direction.getHorizontalDirection().equals("left")) {
-            this.xPos--;
-            this.yPos++;
-        } else if (this.direction.getVerticalDirection().equals("up") && this.direction.getHorizontalDirection().equals("right")) {
-            this.xPos++;
-            this.yPos--;
-        } else if (this.direction.getVerticalDirection().equals("up") && this.direction.getHorizontalDirection().equals("left")) {
-            this.xPos--;
-            this.yPos--;
-        } else if (this.direction.getVerticalDirection().equals("down") && this.direction.getHorizontalDirection().equals("right")) {
-            this.xPos++;
-            this.yPos++;
+        if (direction.getVerticalDirection().equals("down") && direction.getHorizontalDirection().equals("left")) {
+            xPos--;
+            yPos++;
+        } else if (direction.getVerticalDirection().equals("up") && direction.getHorizontalDirection().equals("right")) {
+            xPos++;
+            yPos--;
+        } else if (direction.getVerticalDirection().equals("up") && direction.getHorizontalDirection().equals("left")) {
+            xPos--;
+            yPos--;
+        } else if (direction.getVerticalDirection().equals("down") && direction.getHorizontalDirection().equals("right")) {
+            xPos++;
+            yPos++;
         }
         repaint();
     }
 
-    public void setNewDirection(String horizontal, String vertical) {
-        this.direction.setHorizontalDirection(horizontal);
-        this.direction.setVerticalDirection(vertical);
+    public void setNewDirection(String horizontalDirection, String verticalDirection) {
+        direction.setHorizontalDirection(horizontalDirection);
+        direction.setVerticalDirection(verticalDirection);
     }
 
     public Programming_Exercise_11_17_Direction getDirection() {
-        return this.direction;
+        return direction;
     }
 
     public Dimension getPreferredSize() {
@@ -77,19 +71,19 @@ public class Programming_Exercise_11_17_Component extends JComponent{
     }
 
     public int getXpos() {
-        return this.xPos;
+        return xPos;
     }
 
     public int getYpos() {
-        return this.yPos;
+        return yPos;
     }
 
     public void setXpos(int pos) {
-        this.xPos = pos;
+        xPos = pos;
     }
 
     public void setYpos(int pos) {
-        this.yPos = pos;
+        yPos = pos;
     }
 }
 
