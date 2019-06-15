73d4a0bd21ae6946fe6b513dda7739d4145b8240
Daan Pol <daanpol@tutamail.com>
2019-06-15 10:11:38 +0200

    Added Programming Exercise 11.17 Direction

--- Horstmann_Ch_11_Advanced_User_Interfaces/src/Programming_Exercise_11_17_Direction.java
+++ Horstmann_Ch_11_Advanced_User_Interfaces/src/Programming_Exercise_11_17_Direction.java
@@ -1,25 +1,27 @@
+
 public class Programming_Exercise_11_17_Direction{
 
-    String horizontal_direction, vertical_direction;
+    private String horizontal_direction;
+    private String vertical_direction;
 
     public Programming_Exercise_11_17_Direction(String horizontal, String vertical){
-        this.horizontal_direction=horizontal;
-        this.vertical_direction=vertical;
+        horizontal_direction=horizontal;
+        vertical_direction=vertical;
     }
 
     public void setHorizontalDirection(String horizontal) {
-        this.horizontal_direction = horizontal;
+        horizontal_direction = horizontal;
     }
 
     public void setVerticalDirection(String vertical){
-        this.vertical_direction=vertical;
+        vertical_direction=vertical;
     }
 
     public String getHorizontalDirection(){
-        return this.horizontal_direction;
+        return horizontal_direction;
     }
 
     public String getVerticalDirection(){
-        return this.vertical_direction;
+        return vertical_direction;
     }
 }
