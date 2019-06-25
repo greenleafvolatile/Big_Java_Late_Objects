b744cb94352047c07e726dab070e16c0dd4b9ae1
Daan Pol <daanpol@tutamail.com>
2019-06-25 14:09:55 +0200

    Added Programming Exercise 7.2

---
+++ Horstmann_Ch_07_Input_Output_And_Exception_Handling/src/Programming_Exercise_7_2.java
@@ -0,0 +1,43 @@
+import javax.swing.JOptionPane;
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.IOException;
+
+/**
+ * "Write a program that reads a file containing text. Read each line and send it to the output file, preceded by line numbers."
+ */
+public class programmingExercise72
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            String inputFile = JOptionPane.showInputDialog("Please enter the input filename: ");
+            String outputFile = JOptionPane.showInputDialog("Please enter the output filename: ");
+            File inFile = new File(inputFile);
+            Scanner lineReader = new Scanner(inFile);
+            PrintWriter outFile = new PrintWriter(outputFile);
+            try
+            {
+                int count = 1;
+                while (lineReader.hasNextLine())
+                {
+                    String line = lineReader.nextLine();
+                    outFile.printf("/* %s */ %s%n", count, line);
+                    count ++;
+                }
+            }
+            finally
+            {
+                lineReader.close();
+                outFile.close();
+            }
+        }
+        catch (IOException exception)
+        {
+            exception.printStackTrace();
+        }
+    }
+}
