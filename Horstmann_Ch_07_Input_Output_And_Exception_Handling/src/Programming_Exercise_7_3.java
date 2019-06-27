0733a2699acc8a00e445faa7f09fb78917e8b190
Daan Pol <daanpol@tutamail.com>
2019-06-27 19:23:00 +0200

    Added Programming Exercise 7.3

--- Horstmann_Ch_07_Input_Output_And_Exception_Handling/src/Programming_Exercise_7_3.java
+++ Horstmann_Ch_07_Input_Output_And_Exception_Handling/src/Programming_Exercise_7_3.java
@@ -6,49 +6,39 @@ import java.io.IOException;
 
 /**
  * "Repeat Exercise P7.2, but allow the user to specify the file name in the command-line.
- * If the user doesnt specify any file name, them prompt the user for the name."
+ * If the user doesn't specify any file name, them prompt the user for the name."
  */
 
-public class Programming_Exercise_7_3
-{
-    public static void main(String[] args)
-    {
-        String inputFile = " ";
-        String outputFile = " ";
-        if (args.length < 2)
-        {
-            System.out.print("Please provide 2 files.");
-        }
-        else
-        {
+public class Programming_Exercise_7_3 {
+    public static void main(String[] args) {
+        String inputFile;
+        String outputFile;
+        if (args.length < 2) {
+            System.out.print("Please provide 2 files!\n");
+            System.exit(0);
+        } else {
             inputFile = args[0];
             outputFile = args[1];
-        }
 
-        try
-        {
-            File inFile = new File(inputFile);
-            Scanner lineReader = new Scanner(inFile);
-            PrintWriter outFile = new PrintWriter(outputFile);
-            try
-            {
-                int count = 1;
-                while (lineReader.hasNextLine())
-                {
-                    String line = lineReader.nextLine();
-                    outFile.printf("/* %s */ %s%n", count, line);
-                    count ++;
+            try {
+                File inFile = new File(inputFile);
+                Scanner lineReader = new Scanner(inFile);
+                PrintWriter outFile = new PrintWriter(outputFile);
+                try {
+                    int lineNumber = 1;
+                    while (lineReader.hasNextLine()) {
+                        String line = lineReader.nextLine();
+                        outFile.printf("/* %s */ %s%n", lineNumber, line);
+                        lineNumber++;
+                    }
+                } finally {
+                    lineReader.close();
+                    outFile.close();
                 }
+            } catch (IOException exception) {
+                exception.printStackTrace();
             }
-            finally
-            {
-                lineReader.close();
-                outFile.close();
-            }
-        }
-        catch (IOException exception)
-        {
-            exception.printStackTrace();
         }
     }
 }
+
