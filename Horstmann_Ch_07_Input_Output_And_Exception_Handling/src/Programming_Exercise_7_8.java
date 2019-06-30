fd3454626cf1dc6f8490ab8994fdeb849860201e
Daan Pol <daanpol@tutamail.com>
2019-06-30 18:06:57 +0200

    Added Programming Exercise 7.8

--- Horstmann_Ch_07_Input_Output_And_Exception_Handling/src/Programming_Exercise_7_8.java
+++ Horstmann_Ch_07_Input_Output_And_Exception_Handling/src/Programming_Exercise_7_8.java
@@ -1,38 +1,36 @@
-import java.util.Scanner;
+import javax.swing.*;
 import java.io.File;
 import java.io.IOException;
-import java.util.logging.*;
-import javax.swing.JFileChooser;
-import javax.swing.JOptionPane;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * "Write a program that replaces each line of a file with its reverse".
  */
 
-public class programmingExercise78
+public class Programming_Exercise_7_8
 {
     public static void main(String[] args)
     {
         JOptionPane.showMessageDialog(null, "In the next window please select a file");
         JFileChooser chooser = new JFileChooser();
-        File inFile = null;
         if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
         {
-            inFile = chooser.getSelectedFile();
-            try
-            {
-                Scanner lineReader = new Scanner(inFile);
-                try
+            File inFile = chooser.getSelectedFile();
+            try(Scanner lineReader = new Scanner(inFile)){
+                ArrayList<String> list =new ArrayList<>();
+                while(lineReader.hasNextLine())
                 {
-                    while(lineReader.hasNextLine())
-                    {
-                        String line = lineReader.nextLine();
-                        printReverseLine(line);
-                    }
+                    String line = lineReader.nextLine();
+                    list.add(reverseLine(line));
                 }
-                finally
-                {
-                    lineReader.close();
+                try(PrintWriter writer=new PrintWriter(inFile)){
+
+                    for (String s : list) {
+                        System.out.println(s);
+                        writer.println(s);
+                    }
                 }
             }
             catch(IOException lineReaderMain)
@@ -47,14 +45,14 @@ public class programmingExercise78
     }
 
 
-    public static void printReverseLine (String line)
+    private static String reverseLine(String line)
     {
-        String reverseLine = "";
+        StringBuilder reverseLine = new StringBuilder();
         for (int i = line.length() - 1; i >= 0; i --)
         {
             char ch = line.charAt(i);
-            reverseLine += ch;
+            reverseLine.append(ch);
         }
-        System.out.println(reverseLine);
+        return reverseLine.toString();
     }
 }
