7ea19a08284777c4a3572086fd4fc6529453e579
Daan Pol <daanpol@tutamail.com>
2019-06-30 18:14:33 +0200

    Added Programming Exercise 7.9

--- Horstmann_Ch_07_Input_Output_And_Exception_Handling/src/Programming_Exercise_7_9.java
+++ Horstmann_Ch_07_Input_Output_And_Exception_Handling/src/Programming_Exercise_7_9.java
@@ -2,41 +2,31 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.PrintWriter;
 import java.io.IOException;
-import java.util.logging.Logger;
 import java.util.ArrayList;
 
 /**
  * "Write a program that reads each line in a file, reverses its lines, and writes them to another file."
  */
 
-public class programmingExercise79
+public class Programming_Exercise_7_9
 {
     public static void main(String[] args)
     {
         if (args.length < 1)
         {
             printUsage();
+            System.exit(0);
         }
         else
         {
             File inFile = new File(args[0]);
             File outFile = new File(args[1]);
-            try
-            {
-                PrintWriter writeLines = new PrintWriter(outFile);
-                try
+            try(PrintWriter out = new PrintWriter(outFile)){
+                ArrayList<String> linesRead = readLines(inFile);
+                for (int i = linesRead.size() - 1; i >= 0; i --)
                 {
-                    ArrayList<String> linesRead = readLines(inFile);
-                    for (int i = linesRead.size() - 1; i >= 0; i --)
-                    {
-                        writeLines.write(linesRead.get(i));
-                    }
+                    out.write(linesRead.get(i));
                 }
-                finally
-                {
-                    writeLines.close();
-                }
-
             }
             catch(IOException IOExlineWriter)
             {
@@ -45,25 +35,15 @@ public class programmingExercise79
         }
     }
 
-    public static ArrayList<String> readLines(File file)
+    private static ArrayList<String> readLines(File file)
     {
-        ArrayList<String> linesRead = new ArrayList<String>();
-        try
-        {
-            Scanner lineReader = new Scanner(file);
-            try
+        ArrayList<String> linesRead = new ArrayList<>();
+        try(Scanner in = new Scanner(file)){
+            while(in.hasNextLine())
             {
-               while(lineReader.hasNextLine())
-               {
-                   String line = lineReader.nextLine();
-                   linesRead.add(String.format("%s%n", line));
-               }
+                String line =in.nextLine();
+                linesRead.add(String.format("%s%n", line));
             }
-            finally
-            {
-                lineReader.close();
-            }
-
         }
         catch(IOException IOExlineReader)
         {
@@ -72,8 +52,8 @@ public class programmingExercise79
         return linesRead;
     }
 
-    public static void printUsage()
+    private static void printUsage()
     {
-        System.out.print("Please provided two files. One to read from and one to write to.");
+        System.out.print("Please provide two files. One to read from and one to write to.");
     }
 }
