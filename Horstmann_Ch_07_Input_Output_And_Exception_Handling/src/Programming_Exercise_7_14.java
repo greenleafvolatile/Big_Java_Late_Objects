5fcf40b8536ee85027a8cb136d2730159818f64b
Daan Pol <daanpol@tutamail.com>
2019-07-02 19:30:40 +0200

    Added Programming Exercise 7_14

---
+++ Horstmann_Ch_07_Input_Output_And_Exception_Handling/src/Programming_Exercise_7_14.java
@@ -0,0 +1,39 @@
+import java.net.URL;
+import java.io.PrintWriter;
+
+import java.util.Scanner;
+
+/**
+ * "Using the mechanism described in Special Topic 7.1, wrote a program that reads all data from a web page and writes them to a file. Prompt the user for the web pae URL and the file."
+ */
+public class Programming_Exercise_7_14
+{
+    public static void main(String[] args)
+    {
+
+        System.out.print("Please provide a URL you want to read data from: ");
+        try(Scanner in=new Scanner(System.in)){
+            String url = in.next();
+            URL pageLocation = new URL(url);
+            Scanner urlReader = new Scanner(pageLocation.openStream());
+            System.out.print("Please provide a file to write to: ");
+            String outFile = in.next();
+            PrintWriter out = new PrintWriter(outFile);
+            try {
+                while (urlReader.hasNextLine()) {
+                    out.println(urlReader.nextLine());
+                }
+            }
+            finally{
+                urlReader.close();
+                out.close();
+            }
+        }
+        catch (Exception e)
+        {
+           System.out.print("Caught Exception");
+        }
+
+
+    }
+}
