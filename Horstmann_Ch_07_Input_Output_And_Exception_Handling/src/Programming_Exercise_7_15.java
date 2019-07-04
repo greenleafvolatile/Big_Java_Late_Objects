b3dbb3073c159290b9e2c67a65d2d924a46011b1
Daan Pol <daanpol@tutamail.com>
2019-07-04 18:38:30 +0200

    Added Programming Exercise 7_15

--- Horstmann_Ch_07_Input_Output_And_Exception_Handling/src/Programming_Exercise_7_15.java
+++ Horstmann_Ch_07_Input_Output_And_Exception_Handling/src/Programming_Exercise_7_15.java
@@ -4,18 +4,20 @@ import org.jsoup.Jsoup;
 import org.jsoup.select.Elements;
 
 import org.jsoup.nodes.Element;
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 
 /**
- * "Using the mechanism described in Special Topic 7.1, write a program that reads all data from a web page and prints all hyperlinks.
- * Extra credit if your program can follow the links that it finds and find links in those web pages as well."
+ * "Using the mechanism described in Special Topic 7.1, write a program that reads all data from a web page and prints all
+ * hyperlinks. Extra credit if your program can follow the links that it finds and find links in those web pages as well."
  */
 
 public class Programming_Exercise_7_15 {
 
 
-    private static ArrayList<String> extractLinks(String originating_web_page) throws Exception{
+    private static ArrayList<String> extractLinks(String originating_web_page) throws IOException {
 
         ArrayList<String> results=new ArrayList<>();
         Document htmlDoc=Jsoup.connect(originating_web_page).timeout(0).ignoreHttpErrors(true).ignoreContentType(true).followRedirects(false).get();
@@ -40,9 +42,9 @@ public class Programming_Exercise_7_15 {
         }
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException{
 
-        String url="https://news.opensuse.org";
+        String url="https://github.com";
         ArrayList<ArrayList<String>> results=new ArrayList<>();
         results.add(extractLinks(url));
         for(String link : results.get(0)){
@@ -51,18 +53,19 @@ public class Programming_Exercise_7_15 {
         }
         for(int i=0;i<results.size();i++){
             if(i==0){
-                System.out.println(url + "links to: " );
+                System.out.println(url + " links to: " );
             }
             else{
-                System.out.println(results.get(0).get(i-1) + "links to: ");
+                System.out.println(results.get(0).get(i-1) + " links to: ");
             }
             if(results.get(i).isEmpty()){
-                System.out.println("No links found");
+                System.out.println("No links found\n");
             }
             else{
                 for (int j=0;j<results.get(i).size();j++) {
                     System.out.printf("%d: %s\n", j+1, results.get(i).get(j));
                 }
+                System.out.println();
             }
         }
     }
