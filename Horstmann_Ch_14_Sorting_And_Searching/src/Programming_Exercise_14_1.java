84667b7ee15a85758ff1dac3cd5fd4c7ab7d9d6c
Daan Pol <daanpol@tutamail.com>
2019-07-14 11:13:08 +0200

    Added Programming Exercise 14.1

--- Horstmann_Ch_14_Sorting_And_Searching/src/Programming_Exercise_14_1.java
+++ Horstmann_Ch_14_Sorting_And_Searching/src/Programming_Exercise_14_1.java
@@ -5,31 +5,40 @@ import java.util.Arrays;
  */
 public class Programming_Exercise_14_1 {
 
-    public static void main(String[] args) {
+    public static void reverseSort(int[] array){
 
-        int[] array = {2, 7, 9, 1};
-        for (int i = 0; i <array.length; i++) {
+        for(int i=0;i<array.length;i++){
             int minPos = findMaxPos(i, array);
             swap(i, minPos, array);
         }
-        System.out.println(Arrays.toString(array));
     }
 
-        private static int findMaxPos(int from, int[] array){
+    private static int findMaxPos(int from, int[] array){
 
-            int maxPos=from;
-            for (int i=from;i<array.length;i++){
-                if(array[i]>array[from]){
-                    maxPos=i;
-                }
+        int maxPos=from;
+        for (int i=from;i<array.length;i++){
+            if(array[i]>array[from]){
+                maxPos=i;
             }
-            return maxPos;
         }
+        return maxPos;
+    }
 
-        private static void swap(int i, int maxPos, int[] array){
+    private static void swap(int i, int maxPos, int[] array){
 
-            int temp=array[maxPos];
-            array[maxPos]=array[i];
-            array[i]=temp;
-        }
+        int temp=array[maxPos];
+        array[maxPos]=array[i];
+        array[i]=temp;
+    }
+
+    public static void main(String[] args) {
+
+
+        int[] array = {2, 7, 9, 1};
+        System.out.println("Before sort: " + Arrays.toString(array));
+        Programming_Exercise_14_1.reverseSort(array);
+        System.out.println("After sort: " + Arrays.toString(array));
+    }
+
+
 }
