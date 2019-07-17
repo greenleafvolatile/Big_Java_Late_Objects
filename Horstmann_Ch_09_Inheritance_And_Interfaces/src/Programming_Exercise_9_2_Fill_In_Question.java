3a351c989ea72d6021796c32da48f58aa909d5b6
Daan Pol <daanpol@tutamail.com>
2019-07-17 12:45:49 +0200

    Added Programming Exercise 9.2

--- Horstmann_Ch_09_Inheritance_And_Interfaces/src/Programming_Exercise_9_2_Fill_In_Question.java
+++ Horstmann_Ch_09_Inheritance_And_Interfaces/src/Programming_Exercise_9_2_Fill_In_Question.java
@@ -1,6 +1,6 @@
 /**
- * "Add a class FillInQuestion to the questionWithAnswerFilledIn hierarchy of Section 9,1. Such a questionWithAnswerFilledIn is constructed with a string that contains
- * the answer, surrounded by _ _, for example, "The inventor of Java was _James Gosling_". The questionWithAnswerFilledIn should be displayed as
+ * "Add a class FillInQuestion to the question hierarchy of Section 9,1. Such a question is constructed with a string that contains
+ * the answer, surrounded by _ _, for example, "The inventor of Java was _James Gosling_". The question should be displayed as
  * "The inventor of Java was ____________"."
  */
 
