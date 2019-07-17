aae86477b054bdb9c4d1608fc8503ffd4f63b749
Daan Pol <daanpol@tutamail.com>
2019-07-17 12:09:47 +0200

    Added Programming Exercise 14.2

--- Horstmann_Ch_14_Sorting_And_Searching/src/Programming_Exercise_14_2.java
+++ Horstmann_Ch_14_Sorting_And_Searching/src/Programming_Exercise_14_2.java
@@ -6,41 +6,48 @@ import java.util.Arrays;
 
 public class Programming_Exercise_14_2 {
 
+    public class Programming_Exercise_14_2_Coin {
+        private int value;
+        private String name;
+
+        private Programming_Exercise_14_2_Coin(int aValue, String aName) {
+            value = aValue;
+            name = aName;
+        }
+
+        private int getValue() {
+            return value;
+        }
+
+        public String toString() {
+            return name;
+        }
+    }
+
     public static void main(String[] args) {
 
         Programming_Exercise_14_2_Coin[] coins = new Programming_Exercise_14_2_Coin[5];
-        coins[2] = new Programming_Exercise_14_2_Coin(1, "cent");
-        coins[3] = new Programming_Exercise_14_2_Coin(5, "nickel");
-        coins[4] = new Programming_Exercise_14_2_Coin(10, "dime");
-        coins[1] = new Programming_Exercise_14_2_Coin(25, "penny");
-        coins[0] = new Programming_Exercise_14_2_Coin(100, "dollar");
+        coins[2] = new Programming_Exercise_14_2().new Programming_Exercise_14_2_Coin(1, "cent");
+        coins[3] = new Programming_Exercise_14_2().new Programming_Exercise_14_2_Coin(5, "nickel");
+        coins[4] = new Programming_Exercise_14_2().new Programming_Exercise_14_2_Coin(10, "dime");
+        coins[1] = new Programming_Exercise_14_2().new Programming_Exercise_14_2_Coin(25, "penny");
+        coins[0] = new Programming_Exercise_14_2().new Programming_Exercise_14_2_Coin(100, "dollar");
 
         System.out.println("Before sort: " + Arrays.toString(coins));
 
+        // Selection sort coins by their value.
         for (int i = 0; i < coins.length; i++) {
-            int minPos = findMinPos(i, coins);
-            swap(coins, i, minPos);
+            int minPos = i;
+            for(int j=i;j<coins.length;j++){
+                if(coins[j].getValue()<coins[minPos].getValue()){
+                    minPos=j;
+                }
+            }
+            Programming_Exercise_14_2_Coin temp=coins[minPos];
+            coins[minPos]=coins[i];
+            coins[i]=temp;
         }
 
         System.out.println("After sort: " + Arrays.toString(coins));
     }
-
-    private static int findMinPos(int from, Programming_Exercise_14_2_Coin[] coins){
-        int minPos=from;
-        for(int i = from; i<coins.length; i++){
-            if(coins[i].getValue()<coins[minPos].getValue()){
-                minPos=i;
-            }
-        }
-        return minPos;
-    }
-
-    private static void swap(Programming_Exercise_14_2_Coin[] coins, int i, int minPos){
-
-        Programming_Exercise_14_2_Coin temp=coins[i];
-        coins[i]=coins[minPos];
-        coins[minPos]=temp;
-    }
-
-
 }
