b850e5190409be7ca0bdd57a3e332fa64688a4f8
Daan Pol <daanpol@tutamail.com>
2019-07-18 13:44:48 +0200

    Added Programming Exercise 2.7

--- Horstmann_Ch_02_Fundamental_Data_Types/src/Programming_Exercise_2_7.java
+++ Horstmann_Ch_02_Fundamental_Data_Types/src/Programming_Exercise_2_7.java
@@ -7,16 +7,15 @@ import javax.swing.JOptionPane;
 
 public class Programming_Exercise_2_7 {
 
-    public static void main(String[] args)
+    public static void main(String[] args){
 
-    {
         double radius = Double.parseDouble(JOptionPane.showInputDialog("Please input a radius."));
         double diameter = radius * 2;
         double area = Math.PI * Math.pow(radius, 2);
         double circumference = Math.PI * diameter;
         double volume = 0.75 * Math.PI * Math.pow(radius, 3);
         double surfaceArea = 4 * Math.PI * Math.pow(radius, 2);
-        JOptionPane.showMessageDialog(null, "The radius you enterd is: " + radius);
+        JOptionPane.showMessageDialog(null, "The radius you entered is: " + radius);
         JOptionPane.showMessageDialog(null, String.format("The area of a circle with radius " + radius + " would be %.2f ", area));
         JOptionPane.showMessageDialog(null, String.format("The circumference of a circle with radius " + radius + " would be %.2f", circumference));
         JOptionPane.showMessageDialog(null, String.format("The volume of a sphere with radius " + radius + " would be %.2f", volume));
