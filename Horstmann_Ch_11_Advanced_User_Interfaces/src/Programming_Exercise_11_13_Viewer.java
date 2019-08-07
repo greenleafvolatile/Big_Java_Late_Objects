e46e8428329e9b9215c14b0c0ae2b6979c2ce82b
Daan Pol <daanpol@tutamail.com>
2019-08-07 16:24:09 +0200

    Added Programming_Exercise_11_13_Viewer

--- Horstmann_Ch_11_Advanced_User_Interfaces/src/Programming_Exercise_11_13_Viewer.java
+++ Horstmann_Ch_11_Advanced_User_Interfaces/src/Programming_Exercise_11_13_Viewer.java
@@ -4,7 +4,6 @@ import javax.swing.*;
  * "Change the rectangleComponent for the animation in Section 11.5 so that the rectangle bounces off the
  * edges of the component rather than simply moving outside."
  */
-
 public class Programming_Exercise_11_13_Viewer {
 
     public static void main(String[] args){
