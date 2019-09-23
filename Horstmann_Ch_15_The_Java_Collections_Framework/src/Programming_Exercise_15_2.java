6df2e1a8573aeffcfef7259c8267217e8efaf1e5
Daan Pol <daanpol@tutamail.com>
2019-09-23 21:37:48 +0200

    Added Programming Exercise 15_2

--- Horstmann_Ch_15_The_Java_Collections_Framework/src/Programming_Exercise_15_2.java
+++ Horstmann_Ch_15_The_Java_Collections_Framework/src/Programming_Exercise_15_2.java
@@ -1,15 +1,17 @@
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.ListIterator;
 import java.util.logging.Logger;
 
 /**
  * "Write a method public static void reverse(LinkedList<String> strings) that reverses the entries in a linked list."
+ *
+ *
  */
 public class Programming_Exercise_15_2 {
 
     public static void reverse(LinkedList<String> strings){
-        ListIterator<String> lIt=strings.listIterator();
         int j=strings.size()-1;
         for(int i=0;i<strings.size();i++){
            String string=strings.poll();
@@ -23,7 +25,10 @@ public class Programming_Exercise_15_2 {
         LinkedList<String> employeeNames=new LinkedList<>(Arrays.asList("Mike", "Dave", "George", "Lynn", "Monique", "John", "Samantha", "Pam", "Bernard", "Chris", "Eleanor"));
         reverse(employeeNames);
         for(String string : employeeNames){
+
             System.out.println(string);
         }
+
+        // And then I came across Collections.reverse()l
     }
 }
